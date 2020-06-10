5319d1526f77ef21b4ad80a4b615e5071e205119
bhanujggandhi <gandhibhanuj@gmail.com>
2020-06-10 13:59:30 +0530

    error handled

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,22 @@
 const request = require("request");
 
 const url =
-  "http://api.weatherapi.com/v1/current.json?key=7c932ced4d6b4d41acb85714200806&q=delhi";
+  "http://api.weatherapi.com/v1/forecast.json?key=7c932ced4d6b4d41acb85714200806&q=panipat";
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+  if (error) {
+    console.log("Unable to connect to weather service!");
+  } else if (response.body.error) {
+    console.log(response.body.error.message);
+  } else {
+    console.log(
+      `It is ${
+        response.body.forecast.forecastday[0].day.condition.text
+      } outside in ${response.body.location.name} with ${
+        response.body.current.temp_c
+      } degrees currently. There is a ${
+        response.body.current.precip_mm * 10
+      }% chance of rain.`
+    );
+  }
 });
