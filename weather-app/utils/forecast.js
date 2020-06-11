5a50317b3ac0d43a6b8ca4f177cde80f7b05fa8b
bhanujggandhi <gandhibhanuj@gmail.com>
2020-06-11 18:30:32 +0530

    Object es6 in weatherapp

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -5,20 +5,18 @@ const forecast = (address, callback) => {
     "http://api.weatherapi.com/v1/forecast.json?key=7c932ced4d6b4d41acb85714200806&q=" +
     encodeURIComponent(address);
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to weather service!", undefined);
-    } else if (response.body.error) {
-      callback(response.body.error.message, undefined);
+    } else if (body.error) {
+      callback(body.error.message, undefined);
     } else {
       callback(
         undefined,
-        `It is ${
-          response.body.forecast.forecastday[0].day.condition.text
-        } outside in ${response.body.location.name} with ${
-          response.body.current.temp_c
-        } degrees currently. There is a ${
-          response.body.current.precip_mm * 10
+        `It is ${body.forecast.forecastday[0].day.condition.text} outside in ${
+          body.location.name
+        } with ${body.current.temp_c} degrees currently. There is a ${
+          body.current.precip_mm * 10
         }% chance of rain.`
       );
     }
