baf891578b294a7aedbb1fbfa6d335902d83b1a0
bhanujggandhi <gandhibhanuj@gmail.com>
2020-06-12 17:20:13 +0530

    weather details rendered

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,28 @@
 console.log("client side js file is loaded");
 
-fetch("http://localhost:3000/weather?address=!").then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      return console.log(data.error);
-    }
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#location");
+const messageTwo = document.querySelector("#forecast");
 
-    console.log(data.location);
-    console.log(data.forecast);
-  });
+weatherForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const location = search.value;
+
+  messageOne.textContent = "loading...";
+  messageTwo.textContent = "";
+
+  fetch("http://localhost:3000/weather?address=" + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          return (messageOne.textContent = data.error);
+        }
+
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      });
+    }
+  );
 });
