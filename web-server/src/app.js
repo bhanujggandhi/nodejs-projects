7bf619fcdff71d04f18e5cfd59090a2ef265eb81
bhanujggandhi <gandhibhanuj@gmail.com>
2020-06-12 14:39:17 +0530

    Query string 101

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -40,16 +40,30 @@ app.get("/help", (req, res) => {
 });
 
 app.get("/weather", (req, res) => {
-  res.send([
-    {
-      name: "Rahul",
-      dialogue: "Naam toh suna hoga",
-    },
-    {
-      name: "Raj",
-      dialgue: "Tum nai smjhogi simran, Kuch kuch hota h",
-    },
-  ]);
+  if (!req.query.address) {
+    return res.send({
+      error: "Please provide the address",
+    });
+  }
+
+  res.send({
+    forecast: "It is raining",
+    location: "Panipat",
+    address: req.query.address,
+  });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
+
+  console.log(req.query);
+  res.send({
+    products: [],
+  });
 });
 
 app.get("/help/*", (req, res) => {
---
+++ web-server/src/utils/forecast.js
@@ -0,0 +1,26 @@
+const request = require("request");
+
+const forecast = (address, callback) => {
+  const url =
+    "http://api.weatherapi.com/v1/forecast.json?key=7c932ced4d6b4d41acb85714200806&q=" +
+    encodeURIComponent(address);
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback(body.error.message, undefined);
+    } else {
+      callback(
+        undefined,
+        `It is ${body.forecast.forecastday[0].day.condition.text} outside in ${
+          body.location.name
+        } with ${body.current.temp_c} degrees currently. There is a ${
+          body.current.precip_mm * 10
+        }% chance of rain.`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
