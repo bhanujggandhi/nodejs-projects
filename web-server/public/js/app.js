39b37660382d500dfd4a1ecf29c0510c3659aa8b
bhanujggandhi <gandhibhanuj@gmail.com>
2020-06-12 16:42:53 +0530

    Fetch API

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log("client side js file is loaded");
+
+fetch("http://localhost:3000/weather?address=!").then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      return console.log(data.error);
+    }
+
+    console.log(data.location);
+    console.log(data.forecast);
+  });
+});
