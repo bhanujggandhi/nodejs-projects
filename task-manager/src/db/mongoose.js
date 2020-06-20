f620b28c499a5194847566ee24e451d8e9df1418
bhanujggandhi <gandhibhanuj@gmail.com>
2020-06-21 02:59:04 +0530

    data validation and sanitization

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -24,6 +24,16 @@ const User = mongoose.model("User", {
       }
     },
   },
+  password: {
+    type: String,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("Password should not contain password");
+      }
+    },
+  },
   age: {
     type: Number,
     default: 0,
@@ -35,38 +45,42 @@ const User = mongoose.model("User", {
   },
 });
 
-const me = new User({
-  name: "        Bhanuj    ",
-  email: "GANDHIBHANUJ@GMAIL.COM        ",
-});
+// const me = new User({
+//   name: "        Ishita    ",
+//   email: "ISHITABATRA22@GMAIL.COM        ",
+//   password: "passwod123",
+//   age: 19,
+// });
 
-me.save()
-  .then(() => {
-    console.log(me);
-  })
-  .catch((error) => {
-    console.log("Error ", error);
-  });
+// me.save()
+//   .then(() => {
+//     console.log(me);
+//   })
+//   .catch((error) => {
+//     console.log("Error ", error);
+//   });
 
 const Task = mongoose.model("Task", {
   description: {
     type: String,
+    trim: true,
+    required: true,
   },
   completed: {
     type: Boolean,
+    default: false,
   },
 });
 
-// const task1 = new Task({
-//   description: "Fill internship form",
-//   completed: true,
-// });
+const task1 = new Task({
+  description: "                            Fill internship form",
+});
 
-// task1
-//   .save()
-//   .then(() => {
-//     console.log(task1);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
+task1
+  .save()
+  .then(() => {
+    console.log(task1);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
