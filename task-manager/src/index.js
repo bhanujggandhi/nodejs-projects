bf82fc4897b70542418e22d25ba47aaca91fb90b
bhanujggandhi <gandhibhanuj@gmail.com>
2020-06-25 18:00:13 +0530

    Update route setup

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -45,6 +45,35 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
+app.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+// ==============================Task Routes==================================
+
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
 
@@ -78,6 +107,33 @@ app.get("/tasks/:id", async (req, res) => {
     res.send(task);
   } catch (err) {
     res.status(500).send(err);
+  }
+});
+
+app.patch("/tasks/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["description", "completed"];
+  const isValidUpdate = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidUpdate) {
+    return res.status(400).send({ error: "Invalid update" });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (err) {
+    res.status(400).send(err);
   }
 });
 
