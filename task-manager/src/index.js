bd07cc9b95084a2213b5a064b7a315df443ea6ee
bhanujggandhi <gandhibhanuj@gmail.com>
2020-06-30 12:10:54 +0530

    middleware 101

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,6 +6,24 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     res.send("GET requests are disabled");
+//   } else {
+//     next();
+//   }
+// });
+
+app.use((req, res, next) => {
+  if (req.method) {
+    res
+      .status(503)
+      .send(
+        "This site is in maintainance mode. Please try again after sometime"
+      );
+  }
+});
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
