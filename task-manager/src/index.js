4bf1107fd9e8370bff6b485a22b974b0f5e7de44
bhanujggandhi <gandhibhanuj@gmail.com>
2020-07-02 22:08:27 +0530

    Validation of file uploads

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,7 +7,23 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const multer = require("multer");
-const upload = multer({ dest: "images" });
+const upload = multer({
+  dest: "images",
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return cb(new Error("Please upload a Word document(.doc or .docx)"));
+    }
+
+    cb(undefined, true);
+
+    // cb(new Error('File must be a PDF'))
+    // cb(undefined, true)
+    // cb(undefined, false)
+  },
+});
 
 app.post("/upload", upload.single("upload"), (req, res) => {
   res.send();
