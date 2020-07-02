b03068db52fcc555b317c3212db8c3eb862e4acf
bhanujggandhi <gandhibhanuj@gmail.com>
2020-07-02 16:53:24 +0530

    Pagination setup

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -17,10 +17,29 @@ router.post("/tasks", auth, async (req, res) => {
   }
 });
 
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=(10/20/30)
 router.get("/tasks", auth, async (req, res) => {
+  const match = {};
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === "true";
+  }
+
   try {
-    const tasks = await Task.find({ owner: req.user._id });
-    res.send(tasks);
+    await req.user
+      .populate({
+        path: "tasks",
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+        },
+      })
+      .execPopulate();
+    res.send(req.user.tasks);
+    // const tasks = await Task.find({ owner: req.user._id, completed: false });
+    // res.send(tasks);
   } catch (err) {
     res.status(500).send();
   }
