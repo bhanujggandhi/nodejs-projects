b4bff77547342b4077e78c82e14e3d7e35bdf114
bhanujggandhi <gandhibhanuj@gmail.com>
2020-07-07 02:50:51 +0530

    Send-grid messages setup

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -5,16 +5,34 @@ const sendgridAPIKey =
 
 sgMail.setApiKey(sendgridAPIKey);
 
-sgMail
-  .send({
-    to: "gandhibhanuj@gmail.com",
-    from: "task-app@bhanujgandhi.tech",
-    subject: "This is my first creation",
-    text: "I hope this one actually gets to you",
-  })
-  .then(() => {
-    console.log("sent");
-  })
-  .catch((err) => {
-    console.log(err);
+const sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: "tasker-by-bhanuj@bhanujgandhi.tech",
+    subject: "Thanks for joining in!",
+    text: `
+    Welcome to the family, ${name}!
+    
+    It turns out, our brains are actually wired to keep us thinking about our unfinished tasks. Handy when you have one thing you need to work on. Not so good when you have 30+ tasks vying for your attention at once.
+    
+    That’s why the first step to organizing your work and life is getting everything out of your head and onto your to-do list. From there you can begin to organize and prioritize so you know exactly what to focus on and when.
+
+    Yours sincerely,
+    Bhanuj Gandhi
+    `,
   });
+};
+
+const sendCancelationEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: "bhanuj-tasker@bhanujgandhi.tech",
+    subject: "Sorry to see you go.",
+    text: `Goodbye, ${name}. I hope to see you back sometime soon.`,
+  });
+};
+
+module.exports = {
+  sendWelcomeEmail,
+  sendCancelationEmail,
+};
