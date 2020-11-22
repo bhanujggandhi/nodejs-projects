b45b7a144b19c029c70dc92e3984776c0156f4c0
bhanujggandhi <gandhibhanuj@gmail.com>
2020-11-22 13:44:02 +0530

    create task tested

--- task-manager/tests/task.test.js
+++ task-manager/tests/task.test.js
@@ -1,6 +1,22 @@
 const request = require("supertest");
+const app = require("../src/app");
 const Task = require("../src/models/task");
+const { userOne, userOneId, setupDatabase } = require("./fixtures/db");
+
+beforeEach(setupDatabase);
 
 describe("Task Operations", () => {
-  it("should create task for user", () => {});
+  it("should create task for user", async () => {
+    const response = await request(app)
+      .post("/tasks")
+      .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+      .send({
+        description: "From my test",
+      })
+      .expect(201);
+
+    const task = await Task.findById(response.body._id);
+    expect(task).not.toBeNull();
+    expect(task.completed).toEqual(false);
+  });
 });
